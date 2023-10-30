320cd05fa1533e9fe079ba1a423b88116d385a03
Tobetiger <iamtobeconfidential@gmail.com>
2023-10-30 09:25:11 +0100

    version 2

--- src/App.js
+++ src/App.js
@@ -1,26 +1,10 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import './App.css';
-import { AiOutlineMenu } from 'react-icons/ai';
 
 function App() {
   return (
-    // Navbar Section
-    <div>
-      <div className='Navbar'>
-      <div>
-      <AiOutlineMenu />
-      </div>
-    <h1>
-      io<span>Topia</span>
-    </h1>
-    <div>
-      <p>Home</p> <p>Affiliate</p>
-    </div>
-
-      </div>
-      
-    </div>
+    <Navbar />
   );
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,38 @@
 import React from 'react'
+import { AiOutlineMenu, AiOutlineSearch } from 'react-icons/ai';
+
 
 const Navbar = () => {
   return (
-    <div>
+    // Navbar Section (left side)
+    <div className='max-w-[1640px] mx-auto flex justify-between item-center p-4'
+    >
+      <div className='flex item-center'>
+      <div className='cursor-pointer p-2'>
+      <AiOutlineMenu size={30} />
+      </div>
+    <h1 className='text-2xl sm:text-3xl lg:text-4xl px-2'>
+      Travel<span className='font-bold'>Topia</span>
+    </h1>
+    <div className='hidden lg:flex item-center bg-gray-200 rounded-full p-1 text-[14px]'>
+      <p className='bg-black rounded-full p-2 text-white'>Home</p> 
+      <p className='p-2 '>Affiliate</p>
+    </div>
+
+      </div>
+
+    {/*Navbar Section (search Input)*/}
+    <div className='bg-gray-200 rounded-full flex item-center px-1 w-[100px] sm:w-[200px] lg:w-[300px] '>
+      <div className='p-3'>
+      <AiOutlineSearch size={20} />
+      </div>
       
+      <input className='bg-transparent p-2 focus:outline-none w-full'  type="text" placeholder='Search Travel Location'>
+      </input>
+
+    </div>
+
+
     </div>
   )
 }
